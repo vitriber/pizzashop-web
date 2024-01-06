757efb434a228eb1485fce9683349525f4c98764
Vitor Ribeiro <vitorribeiro@MBP-de-Vitor.home>
2024-01-06 19:55:36 +0000

    feat: add eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import { Button } from './components/ui/button'
 import './global.css'
 
 export function App() {
-
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className="flex justify-center items-center h-screen">
       <Button> Enviar </Button>
     </div>
   )
